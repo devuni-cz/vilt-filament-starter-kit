64e3629f65df4475f20025633f9d8c59c39274f5
David Janeček <davidjanecek123@seznam.cz>
2025-07-30 13:00:02 +0200

    chore: app.blade.php title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title inertia>{{ config('app.name', 'vilt-starter-kit') }}</title>
+    <title inertia>{{ config('app.name', 'vilt-filament-starter-kit') }}</title>
     @routes
     @vite(['resources/js/app.js', "resources/js/pages/{$page['component']}.vue"])
     @inertiaHead
